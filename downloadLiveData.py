f36cea04475215e8896f9a888f57da4749a3facc
David Huang <42119351+hiydavid@users.noreply.github.com>
2022-10-25 23:45:29 -0700

    Updated workflow.yaml

--- downloadLiveData.py
+++ downloadLiveData.py
@@ -2,6 +2,6 @@
 import pandas as pd
 from numerapi import NumerAPI
 
-napi = NumerAPI(public_id=PUBLIC_ID, secret_key=SECRET_KEY)
+napi = NumerAPI(public_id, secret_key)
 current_round = napi.get_current_round()
 print(f"Current round #: {current_round}")
