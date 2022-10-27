e682ebd4f310d3e303d924550a9f882db69de4bb
David Huang <42119351+hiydavid@users.noreply.github.com>
2022-10-27 00:18:08 -0700

    Simplified workflow.yaml

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
+# download numerai live data
+
+# print
+print("DAILY PROCESS BEGIN!")
+
 # load libraries
 import os
 import gc
+import json
 from numerapi import NumerAPI
 from utils.models import RunModel
 
@@ -8,10 +14,16 @@ from utils.models import RunModel
 PUBLIC_ID = os.getenv('PUBLIC_ID')
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# instantiate api & check for round
+# instantiate numerai client
 napi = NumerAPI(public_id=PUBLIC_ID, secret_key=SECRET_KEY)
 current_round = napi.get_current_round()
 print(f"Current round #: {current_round}")
+
+# donwload live dataset
+napi.download_dataset(
+    filename="v4/live.parquet", 
+    dest_path=f"data/live_{current_round}.parquet"
+)
 
 # init class
 nmr = RunModel(roundn=current_round, mode="live")
@@ -49,3 +61,17 @@ gc.collect()
 nmr.run_terra()
 gc.collect()
 
+# read model name json file
+with open("data/model_names.json", "r") as f:
+    model_names = json.load(f)
+
+# submit live predictions for current round
+for item in model_names.items():
+    print(f"Submitting live predictions for {item[0]}...")
+    napi.upload_predictions(
+        file_path=f"predictions/{item[0]}_live_preds_{current_round}.csv",
+        model_id=item[1]
+    )
+
+# print
+print("DAILY PROCESS COMPLETE!")
